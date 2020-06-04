977e6a64e21a127a9b254df246eb561949be0c91
Vincent Bohlen <vincent.bohlen@fokus.fraunhofer.de>
2020-06-04 13:25:57 +0200

    Config files cleaned up

--- config/runtime-config.js
+++ config/runtime-config.js
@@ -1,4 +1,3 @@
 export default {
     apiBaseUrl: '$VUE_APP_BACKEND_BASE_URL',
-    configManagerUrl: '$VUE_APP_CONFIG_MANAGER_BASE_URL'
 }
--- config/user-config.js
+++ config/user-config.js
@@ -1,6 +1,5 @@
 const glueConfig = {
     apiBaseUrl: 'http://localhost:8090',
-    configManagerUrl: 'http://localhost:8091',
 }
 
 export { glueConfig };
